425bdf147dfd6ae344e5204b4c89d0e5069b1d6c
maple <maple@EbonyMapleTree.net>
2025-11-04 23:16:24 +0000

    fix gitignore

--- build-web/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maple/Documents/WebTest/build-web/_deps/raylib-src/src
-# Build directory: /home/maple/Documents/WebTest/build-web/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CMakeFiles/example.dir/src/data/test.png.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/example.dir/src/data/test.png.c.o: \
- /home/maple/Documents/WebTest/src/data/test.png.c \
- /usr/include/stdc-predef.h
